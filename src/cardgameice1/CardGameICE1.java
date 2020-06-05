fe5be7464b78b8f43101aab24a6a7f8578e1a782
KiratMehta <mehtkira@sheridancollege.ca>
2020-06-05 19:14:30 -0400

    interactive..

--- src/cardgameice1/CardGameICE1.java
+++ src/cardgameice1/CardGameICE1.java
@@ -20,16 +20,24 @@ public class CardGameICE1
     public static void main(String[] args)
     {
         // code to generate 7 random cards and store in array
-        double comp=Math.floor(Math.random()*13+1);
-        System.out.println(comp);
+        int comp= (int)Math.floor(Math.random()*13+1);
+        int comp2=(int)Math.floor(Math.random()*4+1);
+        
+        Card obj=new Card();
+        
+//        System.out.println(comp);
+//        System.out.println(comp2);
         Card[] magicHand = new Card[7];//array of object
         for (int i=0;i<magicHand.length;i++)
         {
            Card c= new Card();//object
-        // c.setValue(method to generate random value from 1 to 13);
+         c.setValue(comp);
+         //  c.setValue(method to generate random value from 1 to 13);
          //  c.setSuits(method random suits);
            magicHand[i]=c;//saving object in array
+           System.out.println(magicHand[i]);
         }
+       
     //take input from user and compare with array 
    // value=
        //     suit =
