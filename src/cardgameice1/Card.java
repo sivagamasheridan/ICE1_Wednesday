f81e3989d08b20675c9227f0f27443abcd999806
Randelle Saltat <50712722+saltatj@users.noreply.github.com>
2020-06-05 14:47:10 -0400

    Add files via upload

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -1,45 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cardgameice1;
+
+import java.util.Random;
 
 /**
  *
- * @author srinivsi
+ * @author saltatj
+ * @date 2020-06-04
  
  private variable then refactor it
  * - encapsulate fields (encapsulation principle) 
  getters/setters
  */
+
 public class Card 
 {
   private String suits;//diamonds,clubs,spades, hearts
   private int value; // 1 to 13
-public static final String[] SUITS={"diamonds","clubs","spades","hearts"};
+  public static final String[] SUITS={"diamonds","clubs","spades","hearts"};
     
     public String getSuits() {
         return suits;
     }
-
-    
+  
     public void setSuits(String suits) {
         this.suits = suits;
     }
-
-    
+   
     public int getValue() {
         return value;
     }
 
-    
     public void setValue(int value) {
         this.value = value;
+    }    
+    
+    //Randomly Generates a magic card 
+    public Card createMagicCard(){
+        this.setValue(randomCardValue());
+        this.setSuits(randomSuit());
+        
+        return this;
     }
-//public int randomValue()
-//{
-    //int value= // 
-           // return value;
-//}
+    
+    //randomly picks a value from 1-13
+    public int randomCardValue(){
+         Random r = new Random();
+            int min = 1;
+            int max = 13;
+         
+            value = r.nextInt(max-min) + min;
+           
+           return value;
+    }
+    //Randomly picks a suit from SUIT array
+    public String randomSuit(){
+        Random r = new Random();
+            int min = 0;
+            int max = 3;
+         
+            int index = r.nextInt(max-min) + min;
+            String suit = SUITS[index];
+        return suit;
+    }
+    
+
 }
+
--- src/cardgameice1/CardGameICE1.java
+++ src/cardgameice1/CardGameICE1.java
@@ -1,37 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cardgameice1;
 
 /**
  *
- * @author srinivsi
+ * @author saltatj
+ * @date 2020-06-04
  print 7 cards (values,suits) and takes input 
  then prints if matching card is found in an array
  */
-public class CardGameICE1
+import java.util.Scanner;
+
+public class CardGameICE1 
 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args)
-    {
+    { 
+
         // code to generate 7 random cards and store in array
         Card[] magicHand = new Card[7];//array of object
         for (int i=0;i<magicHand.length;i++)
-        {
-           Card c= new Card();//object
-         //  c.setValue(method to generate random value from 1 to 13);
-         //  c.setSuits(method random suits);
+        {         
+           Card c = new Card();//object
+           c.createMagicCard(); 
+           System.out.println(c.getSuits()+ c.getValue());
            magicHand[i]=c;//saving object in array
-        }
-    //take input from user and compare with array 
-   // value=
-       //     suit =
+     }
+        
+       
+    Scanner input = new Scanner(System.in);
+    //asks user to input Suit and Value Number  
+  
+    System.out.println("Pick a value(1-13):");
+    int value = input.nextInt();
+  
+    System.out.println("Pick a suit(diamonds,clubs,spades,hearts):");
+    input.nextLine();
+    String suit = input.nextLine();
     
+    //if player chooses rigth card, break out of loop
+    if (checkCard(magicHand, value, suit)){
+        System.out.println("You picked a magic card!");
+    }
+    else
+         System.out.println("Sorry, You didnt picked a magic card!");
     }
     
+  
+     //Check user's card to see if its a Magic Card
+    public static boolean checkCard(Card[] magicHand ,int value, String suit)
+    {
+   for (int i=0;i<magicHand.length;i++){
+       if (value == magicHand[i].getValue() && suit.equalsIgnoreCase(magicHand[i].getSuits()))
+        {
+         return true;
+        }
+       }
+   return false;
+     
+ }
 }
+
+    
