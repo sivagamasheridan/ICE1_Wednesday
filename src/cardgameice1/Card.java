8ba718f9152fe04c9fa280325beba9345bc1fa86
KunalSharma95 <66033989+KunalSharma95@users.noreply.github.com>
2020-06-05 16:31:13 -0400

    Update Card.java

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -11,14 +11,14 @@ public class Card
   private String suit;//diamonds,clubs,spades, hearts
   private String value; // 1 to 13
 public static final String[] SUITS={"diamonds","clubs","spades","hearts"};
-public static final String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};   
+public static final String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};  // works better as String than int
      Random r= new Random();
-    int randomValue=r.nextInt(SUITS.length);
+    int randomValue=r.nextInt(SUITS.length); // creates 2 different variables I can use to run through the arrays as an index value
     int randomValue2 = r.nextInt(values.length);
     
 
 public String getSuits() {
-    suit = SUITS[randomValue];   
+    suit = SUITS[randomValue];   // tried Math.random()*4 earlier but it didn't work so I had to import java util Random
     return suit;
     }
 
