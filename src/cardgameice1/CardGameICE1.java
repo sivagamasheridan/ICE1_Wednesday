bb6bb2cc773858876386986197f53f6aba688bda
KunalSharma95 <66033989+KunalSharma95@users.noreply.github.com>
2020-06-05 16:03:32 -0400

    Update CardGameICE1.java

--- src/cardgameice1/CardGameICE1.java
+++ src/cardgameice1/CardGameICE1.java
@@ -12,7 +12,7 @@ public class CardGameICE1
     public static void main(String[] args)
     {        
         Scanner in = new Scanner(System.in);
-        int count = 0;
+        int count = 0; // used to keep score
         
         Card[] magicHand = new Card[7];
         for (int i=0;i<magicHand.length;i++)
@@ -22,17 +22,17 @@ public class CardGameICE1
            c.setSuits("");        
            
            magicHand[i]=c;//saving object in array                              
-        System.out.println("Enter a card suit followed by 'of' and then its corresponding rank!");
+        System.out.println("Enter a card suit followed by 'of' and then its corresponding rank!"); //Instructions for user.
        String guessHand= in.nextLine();
-        if (guessHand.equals(c.toString())) {
+        if (guessHand.equals(c.toString())) { // converts Object to string, necessary for comparison
           System.out.println("You guessed correctly!");
-          count++;
+          count++; // updates score
         }else {
-             System.out.println("Incorrect! The card was actually " + c);
+             System.out.println("Incorrect! The card was actually " + c); //reveals card.
         }
         }
          
-         System.out.println("You guessed " + count + " cards correctly!");
+         System.out.println("You guessed " + count + " cards correctly!"); // displays score at the end of loop
     
     }
     
