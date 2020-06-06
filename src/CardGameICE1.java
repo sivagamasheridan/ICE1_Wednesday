5f9ebf7b10c640b43cba9f97e7720e833b33b81b
Gurjit Singh <66034001+sin10490@users.noreply.github.com>
2020-06-05 22:14:20 -0400

    Update CardGameICE1.java

--- src/CardGameICE1.java
+++ src/CardGameICE1.java
@@ -1,6 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * Modifier: Gurjit Singh
+ * Student ID: 991592634
  * and open the template in the editor.
  */
 package cardgameice1;
