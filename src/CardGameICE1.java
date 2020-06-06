358b86ed4a905f51f1e31df7bfcc80305e92e75c
Gurjit Singh <66034001+sin10490@users.noreply.github.com>
2020-06-05 23:03:27 -0400

    Update CardGameICE1.java

--- src/CardGameICE1.java
+++ src/CardGameICE1.java
@@ -1,49 +1,54 @@
 /*
- * Modifier: Gurjit Singh
+Modifier: Gurjit Singh
  * Student ID: 991592634
- * and open the template in the editor.
  */
-package cardgameice1;
+
+package assignment;
+
+import java.util.Scanner;
 
 /**
+ * This class ---Insert Description Here---
  *
- * @author srinivsi
- print 7 cards (values,suits) and takes input 
- then prints if matching card is found in an array
+ * @author Gurjit Singh
  */
-public class CardGameICE1
-{
+public class Assignment {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
         // code to generate 7 random cards and store in array
         Card[] magicHand = new Card[7];//array of object
         for (int i=0;i<magicHand.length;i++)
         {
            Card c= new Card();//object
           c.setValue((int)(Math.random()*13)+1);
-          c.setSuits(c.SUIT[(int)(Math.random()*4)]);
+          c.setSuits(c.SUIT((int)(Math.random()*4)));
            magicHand[i]=c;//saving object in array
          System.out.println(c.getSuits()+""+c.getValue());
         }
      int value;
      String suit;
+     Scanner input = new Scanner(System.in);
      System.out.println("Please enter suit?");
      suit = input.nextLine();
      System.out.println("Please enter a value?");
      value = input.nextInt();
-     boolean flag = flase;
-     for(int i = 0; i< magicHand.length, i++){
-      if(value==magicHand[i].getValue()&&suit.equals(magicHand[i].getSuits()){
-       System.out.println("Your card is in the array");
-       flag = true;
-      }
-         }
-         if(flag ==flase)
+     boolean flag = false;
+     for(int i = 0; i< magicHand.length; i++)
+     {
+      if(value==magicHand[i].getValue()&&suit.equals(magicHand[i].getSuits()))
+        {
+            System.out.println("Your card is in the array");
+            flag = true;
+        }
+     }
+         if(flag ==false)
          System.out.println("Sorry your card is not in the Array?");
     }
-    
-}
+    }
+
+
