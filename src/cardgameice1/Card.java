7297cacffdb7e420a30cbc3d1634d1adeda221e8
Gurjit Singh <66034001+sin10490@users.noreply.github.com>
2020-06-05 21:39:39 -0400

    Update Card.java

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Modifier: Gurjit Singh
+ * Student ID: 991592634
  */
 package cardgameice1;
 
