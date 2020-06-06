f61ec1b993c37bd8137e4d6417c01ffa9647a6c8
aditi <aditi@192.168.2.13>
2020-06-05 22:19:07 -0400

    AditiComit

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -11,7 +11,7 @@ package cardgameice1;
  
  private variable then refactor it
  * - encapsulate fields (encapsulation principle) 
- betters/setters
+ getters/setters
  */
 public class Card 
 {
@@ -19,32 +19,31 @@ public class Card
   private int value; // 1 to 13
 public static final String[] SUITS={"diamonds","clubs","spades","hearts"};
     
+    public String getSuits() {
+        return suits;
+    }
 
+    
+    public void setSuits(String suits) {
+       
+        this.suits = suits;
+        
+    }
+   
     
     public int getValue() {
         return value;
     }
-    public Card(int value, String suit){
-        this.value = value;
-        this.suits = suits;
-    }
-    /**
-     * 
-     * @param value 
-     */
+
     
     public void setValue(int value) {
         this.value = value;
+        
     }
-    
-    public String getSuits(){
-        return this.suits;
+     public int randomCardNum(){
+        return(int)(Math.ceil(Math.random()*13));
     }
-    /**
-     * 
-     * @param suits
-     */
-    public void setSuit(String suits){
-        this.suits = suits;
+    public String randomCardType(){
+    return Card.SUITS[(int)(Math.round(Math.random()*3))];
     }
-}
+    }
---
+++ src/cardgameice1/CardGameICE1.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgameice1;
+
+/**
+ *
+ * @author srinivsi
+ print 7 cards (values,suits) and takes input 
+ then prints if matching card is found in an array
+ */
+import java.util.Scanner;
+public class CardGameICE1
+{
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        int cardNum=0;
+        String cardType;
+        boolean x=false;
+        // code to generate 7 random cards and store in array
+        Card[] magicHand = new Card[7];//array of object
+                for (int i=0;i<magicHand.length;i++)
+        {
+           Card c= new Card();//object
+           c.setValue(c.randomCardNum());
+           c.setSuits(c.randomCardType());
+           magicHand[i]=c;//saving object in array
+           c.getSuits();  
+           c.getValue();
+        }
+
+       
+        System.out.println("Choose type of card(diamond,spade,club and heart):");
+        cardType=input.nextLine();
+        System.out.println("choose a card Number(1-13):");
+        cardNum= input.nextInt();
+       for (int i = 0; i<magicHand.length; i++) {
+            if (cardNum == magicHand[i].getValue() &&
+                    cardType.equalsIgnoreCase(magicHand[i].getSuits())) {
+                x = true;
+                System.out.println("Congrats your card matched.");
+                break;
+            }
+        }
+        if (x== false){
+            System.out.println("Your card didn't match");
+        }
+
+        }
+   
+    
+    }
+    
+
