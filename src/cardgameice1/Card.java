ad76603061eab98e6a3c1161a60af5ae56ceca48
Ratinder Singh bhullar <65683220+Ratinder2315@users.noreply.github.com>
2020-06-05 22:01:10 -0400

    Update Card.java

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -1,4 +1,5 @@
 /*
+ * Student ID: 991585936
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,7 +8,7 @@ package cardgameice1;
 
 /**
  *
- * @author srinivsi
+ * @author Ratidner Singh Bhullar
  
  private variable then refactor it
  * - encapsulate fields (encapsulation principle) 
