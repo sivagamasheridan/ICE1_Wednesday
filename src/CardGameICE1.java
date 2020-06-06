21d4b03e6f5825992497b1f6b696e9b3f5a7c043
harleenkaur991 <66030154+harleenkaur991@users.noreply.github.com>
2020-06-05 22:26:39 -0400

    Update CardGameICE1.java

--- src/CardGameICE1.java
+++ src/CardGameICE1.java
@@ -1,4 +1,4 @@
-/*
+/*991592036
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,7 +7,7 @@ package cardgameice1;
 import java.util.Scanner;
 /**
  *
- * @author srinivsi
+ * @author Harleen Kaur
  print 7 cards (values,suits) and takes input 
  then prints if matching card is found in an array
  */
