9c9cc6ec02d67876abdafb38e22225d8bcde3c23
harleenkaur991 <66030154+harleenkaur991@users.noreply.github.com>
2020-06-05 22:25:42 -0400

    Update Card.java

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -1,4 +1,4 @@
-/*
+/*991592036
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,7 +7,7 @@ package cardgameice1;
 
 /**
  *
- * @author srinivsi
+ * @author Harleen Kaur
  
  private variable then refactor it
  * - encapsulate fields (encapsulation principle) 
